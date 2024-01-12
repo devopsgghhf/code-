0cb7a49f6bec1e059180bd1d0ec88e014fed2230
vishwajeet <142601826+devopsgghhf@users.noreply.github.com>
2024-01-12 14:06:57 +0530

    Update basic code.cpp

--- basic code.cpp
+++ basic code.cpp
@@ -145,34 +145,4 @@ int main(){
     cout<<c;
 	return 0;
 
-	**************break and continue************
-break :-
-#include<iostream>
-using namespace std;
-int main(){
-    for(int i=0;i<20;i++)
-    {
-        cout<<i<<endl;
-        if(i==13){
-            break;
-        }
-    }
-
-}
-	continue:-
-#include<iostream>
-using namespace std;
-int main(){
-    int i;
-    for(i=0;i<=40;i++){
-        if(i==3){
-            continue;
-        }
-        cout<<i<<endl;
-    }
-    return 0;
-}		
-		
-		
 	
-}
