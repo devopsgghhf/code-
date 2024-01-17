0b8bfbd136cff36d0bdb019b09fdc4ed856d6f42
vishwajeet <142601826+devopsgghhf@users.noreply.github.com>
2024-01-17 17:34:16 +0530

    Update funtion.cpp

--- funtion.cpp
+++ funtion.cpp
@@ -1,1 +1,23 @@
+//functions 
+#include<iostream>
+using namespace std;
+//function prototype
+//type function-name(arguments);
+int sum(int a,int b){
+    int c=a+b;
+    return c;4
+    
+    //formal parameters a and b will be taking values from actual parameters are num1 and num2 
+}
+
+int main(){
+    int num1,num2;
+    cout<<"enter 1st no."<<endl;
+    cin>>num1;
+    cout<<"enter 2nd no."<<endl;
+    cin>>num2;
+    cout<<"sum  is="<<sum(num1, num2);
+   return 0;
+}
+
 
